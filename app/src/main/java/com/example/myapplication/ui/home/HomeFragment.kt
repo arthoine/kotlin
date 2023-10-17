9eb9a73df88edc92c574dee37c87aa18bf24b4e7
Antoine <poeuf.alexantoine@orange.fr>
2023-10-17 11:15:12 +0200

    wip note home

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -22,16 +27,36 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val editTextNote: EditText = root.findViewById(R.id.editTextNote)
+        val buttonAdd: Button = root.findViewById(R.id.buttonAdd)
+        val textViewSavedNotes: TextView = root.findViewById(R.id.textViewSavedNotes)
+
+        buttonAdd.setOnClickListener {
+            val noteText = editTextNote.text.toString()
+
+            val sharedPreferences = requireContext().getSharedPreferences("Notes", Context.MODE_PRIVATE)
+
+            // Récupérer toutes les notes existantes depuis les préférences partagées
+            val notesSet = sharedPreferences.getStringSet("notesKey", mutableSetOf()) ?: mutableSetOf()
+
+            // Ajouter la nouvelle note à la liste des notes
+            notesSet.add(noteText)
+
+            // Mettre à jour la liste des notes dans les préférences partagées
+            val editor = sharedPreferences.edit()
+            editor.putStringSet("notesKey", notesSet)
+            editor.apply()
+
+            // Afficher toutes les notes
+            textViewSavedNotes.text = notesSet.joinToString("\n")
         }
+
+        // Reste du code...
+
         return root
     }
 
