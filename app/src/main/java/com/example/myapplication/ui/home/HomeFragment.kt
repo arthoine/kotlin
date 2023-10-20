0651cf7e4ad14d86ee78370597bd27dbb1a394cd
Antoine Poeuf <64365954+arthoine@users.noreply.github.com>
2023-10-20 14:20:28 +0200

    wip note home (#1)
    * wip note home
    
    * wip
    
    * click delete
    
    * wip delete swipe
    
    * remove slide check and reset pos check and reset color check
    
    * end date
    
    * end date
    
    * fix

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,42 +1,111 @@
 package com.example.myapplication.ui.home
 
+import CustomAdapter
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.*
+import android.view.animation.AnimationUtils
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.R
+import android.widget.ListView
+import androidx.core.content.ContextCompat
+import kotlin.math.abs
+
 
 class HomeFragment : Fragment() {
+    private lateinit var listView: ListView
+    private lateinit var notes: MutableList<MutableList<String>>
+    private lateinit var editTitleNote: EditText
+    private lateinit var editTextNote: EditText
+    private lateinit var btnAjouter: Button
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        listView = view.findViewById(R.id.listeNotes)
+        editTitleNote = view.findViewById(R.id.editTitleNote)
+        editTextNote = view.findViewById(R.id.editTextNote)
+        btnAjouter = view.findViewById(R.id.btnAjouter)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        notes = mutableListOf()
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val adapter = CustomAdapter(requireContext(), notes)
+        listView.adapter = adapter
+
+        btnAjouter.setOnClickListener {
+            val title = editTitleNote.text.toString()
+            val note = editTextNote.text.toString()
+
+            if (title.isNotEmpty() && note.isNotEmpty()) {
+                val newNote: MutableList<String> = mutableListOf(title, note)
+                notes.add(newNote)
+                adapter.notifyDataSetChanged()
+                editTitleNote.text.clear()
+                editTextNote.text.clear()
+            } else {
+                Toast.makeText(requireContext(), "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
+            }
         }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        val threshold = 500 // seuil en pixels
+        var selectedView: View? = null
+        var startX = 0f
+        var originalX = 0f
+        var originalY = 0f
+
+        listView.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val position = listView.pointToPosition(motionEvent.x.toInt(), motionEvent.y.toInt())
+                    selectedView = listView.getChildAt(position - listView.firstVisiblePosition)
+
+                    selectedView?.let {
+                        startX = motionEvent.rawX
+                        originalX = it.x
+                        originalY = it.y
+                    }
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    selectedView?.let {
+                        val newX = motionEvent.rawX - startX
+                        it.x = originalX + newX
+
+                        if (abs(newX) > threshold) {
+                            it.setBackgroundColor(Color.RED)
+                            val paillettesAnimation = AnimationUtils.loadAnimation(context, R.anim.paillettes)
+                            it.startAnimation(paillettesAnimation)
+                        } else {
+                            it.setBackgroundColor(Color.WHITE)
+                            it.clearAnimation()
+                        }
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    selectedView?.let {
+                        it.x = originalX
+                        it.y = originalY
+                        it.clearAnimation()
+                        it.setBackgroundColor(Color.WHITE)
+                        selectedView = null
+
+                        val totalXMoved = abs(motionEvent.rawX - startX)
+                        if (totalXMoved > threshold) {
+                            val position = listView.getPositionForView(it)
+                            if (position != AdapterView.INVALID_POSITION) {
+                                notes.removeAt(position)
+                                adapter.notifyDataSetChanged()
+                            }
+                        }
+                    }
+                }
+            }
+            true
+        }
+        return view
     }
 }
